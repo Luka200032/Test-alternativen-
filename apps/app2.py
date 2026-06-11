2b0441abfc0dce20d0975d0811a0d1f08dbe0bfa
Luka <rozinaluka20@gmail.com>
2026-06-11 12:40:45 +0000

    11.6

--- apps/app2.py
+++ apps/app2.py
@@ -1,21 +1,68 @@
-from flask import Flask, render_template, request, redirect, session, jsonify
-from tinydb import TinyDB
+from flask import Flask, render_template, request, redirect, session
+from tinydb import TinyDB, Query
 
-app = Flask(__name__,template_folder="templates2",static_folder="static2")
+app = Flask(__name__)
+app.secret_key = "123"
 
-app.secret_key = "secret123"
+db_users = TinyDB("db2/users.json")
+db_posts = TinyDB("db2/posts.json")
 
-users_db = TinyDB("db/users.json")
-posts_db = TinyDB("db/posts.json")
+@app.route("/")
+def index():
+    if "user" not in session:
+        return redirect("/login")
 
+    posts = db_posts.all()
+    return render_template("template2.html", posts=posts, user=session["user"])
 
-# ---------------- LOGIN ----------------
 @app.route("/login", methods=["GET", "POST"])
 def login():
+    if request.method == "POST":
+        u = request.form["username"]
+        p = request.form["password"]
+
+        User = Query()
+        user = db_users.search((User.username == u) & (User.password == p))
+
+        if user:
+            session["user"] = u
+            return redirect("/")
+        return "napaka login"
+
+    return render_template("template1.html")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        db_users.insert({
+            "username": request.form["username"],
+            "password": request.form["password"]
+        })
+        return redirect("/login")
+
+    # če nimaš register posebej, lahko uporabiš template1 ali ga dodamo kasneje
+    return render_template("template1.html")
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if "user" not in session:
+        return redirect("/login")
 
     if request.method == "POST":
-        username = request.form["username"]
+        db_posts.insert({
+            "text": request.form["text"],
+            "author": session["user"]
+        })
+        return redirect("/")
+
+    return render_template("template3.html")
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect("/login")
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
+    app.run(debug=True)
